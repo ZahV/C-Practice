57a798524a607734d4ce05aaadc326ccefb4eafb
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 13:53:27 +0300

    Add files via upload

---
+++ CountingValleys/CountingValleys/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    internal class Program
+    {
+        private static int CountingValleys(int n, string s)
+        {
+            var terrain = s.ToLower().ToCharArray();
+
+            int valleys = 0;
+
+            int currentLvl = 0;
+
+            for (int i = 0; i < terrain.Length; i++)
+            {
+                if (terrain[i] == 'u')
+                {
+                    currentLvl++;
+                }
+
+                if (terrain[i] == 'd')
+                {
+                    currentLvl--;
+                }
+
+                if (currentLvl == 0 && terrain[i] == 'u')
+                {
+                    valleys++;
+                }
+            }
+
+            return valleys;
+        }
+
+        private static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = CountingValleys(n, s);
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
