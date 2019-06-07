d42f6a474535171f91ab62c2ccaa81f907c5afc6
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 13:47:02 +0300

    Add files via upload

---
+++ ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+
+            Console.WriteLine(numbers.Length);
+        }
+    }
+}
