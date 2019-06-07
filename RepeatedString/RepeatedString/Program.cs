158cc2e9f18cc8a70f539fc6efcaba6afe68fc04
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 14:01:18 +0300

    Add files via upload

---
+++ RepeatedString/RepeatedString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RepeatedString
+{
+    internal class Program
+    {
+        private static double RepeatedString(string s, double n)
+
+        {
+            double counter = 0;
+
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                {
+                    counter++;
+                }
+            }
+            counter = (counter * (n / s.Length));
+            for (int i = 0; i < n % s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+            return Math.Floor(counter);
+        }
+
+        private static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            double n = Convert.ToInt32(Console.ReadLine());
+
+            double result = RepeatedString(s, n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
