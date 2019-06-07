0bb2a1b9db564f42472991a501b8675c95f3994f
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 13:51:53 +0300

    Add files via upload

---
+++ Converter/Converter/Currency.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Converter
+{
+    partial class Program
+    {
+        public class Currency
+        {
+            private double _dollar;
+            
+
+            public double ConvertFrom(string x, double y)
+            {
+                switch (x)
+                {
+                    case "USD":
+                        return _dollar = y * 1;
+                    case "BGN":
+                        return _dollar = y * 0.578202;
+                    case "GBP":
+                        return _dollar = y * 1.292645;
+                    case "EUR":
+                        return _dollar = y * 1.131135;
+
+                }
+
+                return _dollar;
+            }
+
+            public double ConvertTo(string x)
+            {
+                switch (x)
+                {
+                    case "USD":
+                        return _dollar * 1;
+                    case "BGN":
+                        return _dollar * 1.729098;
+                    case "GBP":
+                        return _dollar * 0.773604;
+                    case "EUR":
+                        return _dollar * 0.883945;
+                }
+
+                return _dollar;
+            }
+        }
+    }
+}
---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Converter
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Currency currency = new Currency();
+
+            Console.WriteLine("Please enter a currency to be converted :");
+            string type = Console.ReadLine();
+            Console.WriteLine("Please enter the amount :");
+            double ammount = Double.Parse(Console.ReadLine());
+
+            currency.ConvertFrom(type, ammount);
+
+            Console.WriteLine("Please enter a currency so the previously entered amount can be converted.");
+            string type1 = Console.ReadLine();
+            Console.WriteLine($"{ammount} {type} = {currency.ConvertTo(type1)} {type1}");
+            
+        }
+    }
+}
+ 
