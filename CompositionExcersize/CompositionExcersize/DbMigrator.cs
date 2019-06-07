4b975f305e3de8c659590b2792531df2980db734
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 13:49:44 +0300

    Add files via upload

---
+++ CompositionExcersize/CompositionExcersize/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CompositionExcersize
+{
+    partial class Program
+    {
+
+        public class DbMigrator
+        {
+            private readonly Logger _logger;
+            public DbMigrator(Logger logger)
+            {
+                _logger = logger;
+            }
+
+            public void Migrate()
+            {
+               _logger.Log("we are migrating boyyyyyy");
+            }
+        }
+    }
+}
---
+++ CompositionExcersize/CompositionExcersize/Installer.cs
@@ -0,0 +1,20 @@
+﻿namespace CompositionExcersize
+{
+    partial class Program
+    {
+        public class Installer
+        {
+            private readonly Logger _logger;
+
+            public Installer(Logger logger)
+            {
+                _logger = logger;
+            }
+
+            public void Instal()
+            {
+                _logger.Log("We are installing the application.");
+            }
+        }
+    }
+}
