0667820c385a4c1f7b210748c6d3d87ff4d21ef1
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 14:05:43 +0300

    Add files via upload

---
+++ Stopwatch2/Stopwatch2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Stopwatch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch2 stopwatch = new Stopwatch2();
+            while (true)
+            {
+                if (stopwatch.IsPaused())
+                {
+                    Console.WriteLine("Stopwatch is paused");
+                }
+                else
+                {
+                    Console.WriteLine($"Current time is {stopwatch.CurrentTime.TotalSeconds}");
+                }
+
+                Console.WriteLine("Type 'S' to start and 'T' to stop or 'P' to pause the stopwatch and 'Q' to quit.");
+                string input = Console.ReadLine().ToLower();
+                bool shouldQuit = false;
+
+                switch (input)
+                {
+                    case "s":
+                    case "start":
+                        stopwatch.Start();
+                        break;
+                    case "p":
+                    case "pause":
+                        stopwatch.Pause();
+                        break;
+                    case "t":
+                    case "stop":
+                        Console.WriteLine($"{stopwatch.Stop().TotalSeconds} seconds have elapsed");
+                        break;
+                    case "q":
+                    case "quit":
+                        shouldQuit = true;
+                        break;
+                    case "c":
+                    case "continue":
+                        break;
+                    default:
+                        goto case "c";
+                }
+
+                if (shouldQuit)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch2/Stopwatch2/Stopwatch2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Stopwatch2
+{
+    public class Stopwatch2
+    {
+        private DateTime startTime;
+        private DateTime pausedTime;
+        private TimeSpan pausedSpan;
+
+        private bool isPaused;
+        private bool isStarted;
+
+        public void Start()
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                pausedSpan += DateTime.Now - pausedTime;
+                return;
+            }
+
+            if (isStarted)
+            {
+                Console.WriteLine("You cannot start the Stopwatch a second time. You will have to stop it in order to start it again.");
+                return;
+            }
+
+            isStarted = true;
+            pausedSpan = new TimeSpan();
+            startTime = DateTime.Now;
+        }
+
+        public TimeSpan Stop()
+        {
+            if (!isStarted)
+            {
+                throw new Exception("You cannot stop it before you start it.");
+            }
+
+            var elapsedTime = CurrentTime;
+            isStarted = false;
+            return elapsedTime;
+        }
+
+        public TimeSpan Pause()
+        {
+            if (!isStarted || isPaused)
+            {
+                throw new Exception("You cannot pause it before you start it or if you are already paused.");
+            }
+            pausedTime = DateTime.Now;
+            isPaused = true;
+            return CurrentTime;
+
+        }
+
+        public bool IsPaused()
+        {
+            return isPaused;
+        }
+
+        public TimeSpan CurrentTime
+        {
+            get
+            {
+                if (!isStarted)
+                {
+                    return new TimeSpan();
+                }
+
+                return DateTime.Now - startTime - pausedSpan;
+            }
+        }
+    }
+
+}
