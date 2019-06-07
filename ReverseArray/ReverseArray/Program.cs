68f451540ede34dc06cfbcbeeefa8093fd489a7c
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 14:02:05 +0300

    Add files via upload

---
+++ ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        private static int[] ReverseArray(int[] a)
+        {
+
+            int i = 0;
+            int j = a.Length - 1;
+            while (i < j)
+            {
+                var temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+
+            return a;
+        }
+
+        private static void Main(string[] args)
+        {
+
+            int arrCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+                ;
+            int[] res = ReverseArray(arr);
+
+            Console.WriteLine(string.Join(" ", res));
+
+
+        }
+    }
+}
