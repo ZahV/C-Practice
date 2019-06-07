a4f8f76f75e6e00324899656cff71dfee8523ecf
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 14:07:17 +0300

    Add files via upload

---
+++ UpcastingAndDowncasting/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UpcastingAndDowncasting
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text) shape;
+            
+        }
+    }
+}
---
+++ UpcastingAndDowncasting/UpcastingAndDowncasting/Text.cs
@@ -0,0 +1,8 @@
+﻿namespace UpcastingAndDowncasting
+{
+    public class Text : Shape
+    {
+        public int Fontsize { get; set; }
+        public string FontName {get;set;}
+    }
+}
