c052ed5ef94c39464619738df824592c60d27856
ZahV <43498064+ZahV@users.noreply.github.com>
2019-06-07 13:54:21 +0300

    Add files via upload

---
+++ DataStackWithArray/DataStackWithArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStackWithArray
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var neshtSi = new List<int>();
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(6);
+            stack.Push(9);
+            stack.Push(2);
+            stack.Push(5);
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(8);
+            stack.Push(5);
+            stack.Push(2);
+
+
+            Console.WriteLine($"Popping {stack.Pop().ToString()}");
+            Console.WriteLine($"Peeking {stack.Peek().ToString()}");
+            Console.WriteLine($"Popping {stack.Pop().ToString()}");
+        }
+    }
+}
---
+++ DataStackWithArray/DataStackWithArray/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DataStackWithArray
+{
+
+    public class Stack<T>
+    {
+        private T[] _stack;
+        private int _count;
+      
+
+        public Stack()
+        {
+            _count = 0;
+            _stack = new T[4];
+        }
+
+        public void Push(T value)
+        {
+            if (_count == _stack.Length)
+            {   
+                T[] array2 = new T[_stack.Length*2];
+
+                for (int i = 0; i < _stack.Length; i++)
+                {
+                    for (int j = 0; j < array2.Length; j++)
+                    {
+                        _stack[i] = array2[j];
+                    }
+                }
+
+                _stack = array2;
+               
+            }
+
+            _stack[_count] = value;
+
+            _count++;
+        }
+
+        public T Pop()
+        {
+            _count--;
+            return _stack[_count];
+        }
+
+        public T Peek()
+        {
+            return _stack[_count -1];
+        }
+
+     
+    }
+}
